1ef3b235585bb7382a6c081b222c70d3838e7fde
Varvara694 <varvarakulikova754@gmail.com>
2026-05-13 14:41:06 +0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 class Config:
     SECRET_KEY = 'super_secret_key'
-    SQLALCHEMY_DATABASE_URI = ('postgresql://postgres:root@localhost/korochki_est')
+    SQLALCHEMY_DATABASE_URI = ('postgresql://kulikova_db_user:eFocwZ3YeCxQ3ca85N0QPxHx1wyYUhPt@dpg-d8258prrjlhs738eb2l0-a/kulikova_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
